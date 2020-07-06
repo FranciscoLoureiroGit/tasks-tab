bd608e0f93d995c77ffc21f670b5e26d9b89e11b
FranciscoLoureiroTrab <francisco.loureiro@tecnico.ulisboa.pt>
2020-07-06 17:38:39 +0100

    add/delete tasks implemented

--- oauth.js
+++ oauth.js
@@ -1,9 +1,13 @@
 window.onload = function() {
+    sessionStorage.setItem('tasklists', '');
     getData();
     setInterval(getData,10000);
+    addSelectOnChange();
+    addTaskListener();
 };
 
 function getData(){
+    sessionStorage.removeItem('tasks');
     chrome.identity.getAuthToken({interactive: true}, function(token) {
         let init = {
             method: 'GET',
@@ -20,26 +24,32 @@ function getData(){
             init)
             .then((response) => response.json())
             .then(function(taskslists) {
-                let output = '';
-                let today = new Date().getDay();
+                let tasks = [];
+
                 console.log(taskslists);
-                taskslists.items.forEach(element => fetch(
-                    'https://www.googleapis.com/tasks/v1/lists/' + element.id + '/tasks',
+
+                checkTasklistOrder(taskslists);
+
+                taskslists.items.forEach(element1 => fetch(
+                    'https://www.googleapis.com/tasks/v1/lists/' + element1.id + '/tasks',
                     init)
                     .then((response) => response.json())
-                    .then(function (tasks) {
-                        console.log(tasks);
-                        if(tasks.hasOwnProperty('items')){
-                            output += '<ul>' + element.title + '</ul>';
-                            tasks.items.forEach(element => {
-                                let taskdate = new Date(element.updated);
-                                if (taskdate.getDay() === today){
-                                    output += '<label onclick="completeTask()"><input type="checkbox" class="checkbox">' + element.title + '</label><br></<br><br></<br>'
-                                }
+                    .then(function (data) {
+                        console.log(data);
+                        if(data.hasOwnProperty('items')){
+                            data.items.forEach(element2 => {
+                                tasks.push({
+                                    title: element2.title,
+                                    id: element2.id,
+                                    taskListTitle: element1.title,
+                                    taskListId: element1.id,
+                                    updated: element2.updated,
+                                });
+                                sessionStorage.setItem(element2.id,element1.id);
                             });
+                            sessionStorage.setItem('tasks', JSON.stringify(tasks));
                         }
-                        $('#taskslists').html(output);
-                    })
+                    }).then(printTasks)
                 );
             })
             .catch(error => {
@@ -49,6 +59,143 @@ function getData(){
     });
 }
 
-function completeTask() {
+function completeTask(id) {
+    let taskListId = sessionStorage.getItem(id);
+    console.log(sessionStorage)
 
+    const Url = "https://www.googleapis.com/tasks/v1/lists/" + taskListId + "/tasks/" + id;
+
+
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        let init = {
+            method: 'DELETE',
+            async: true,
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+        };
+
+        fetch(Url, init)
+            .then(response => {
+                if (!response.ok){
+                    console.error("error!")
+                }
+            });
+    });
+    getData();
+}
+
+function addTask(taskListId, title) {
+    const Url = "https://www.googleapis.com/tasks/v1/lists/" + taskListId + "/tasks";
+    let newtask = {
+        title: title
+    };
+
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        let init = {
+            method: 'POST',
+            async: true,
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newtask),
+            'contentType': 'json'
+        };
+
+        fetch(Url, init)
+            .then(response => {
+                if (!response.ok){
+                    console.error("error!")
+                }
+            }).then(getData);
+    });
+
+}
+
+function compare( a, b ) {
+    if ( a.taskListId < b.taskListId ){
+        return -1;
+    }
+    if ( a.taskListId > b.taskListId ){
+        return 1;
+    }
+    if ( a.taskListId === b.taskListId ){
+        if ( a.id < b.id ){
+            return -1;
+        }
+        if ( a.id > b.id ){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function printTasks(){
+    let output = '';
+
+    let tasklist = document.getElementById('list').value;
+    let tasks = JSON.parse(sessionStorage.getItem('tasks'));
+
+    if (tasks != null) {
+        tasks.sort(compare);
+        tasks.forEach(element => {
+            if (element.taskListId === tasklist){
+                output += '<br></<br><label  class="' + element.id + '" id="task" ><input name="task" type="checkbox" class="checkbox">' + element.title + '</label><br></<br>'
+            }
+        })
+    }
+    $('#taskslists').html(output);
+    completeTaskListener();
+}
+
+function completeTaskListener() {
+    let tasks = document.getElementsByName('task');
+
+    // onClick's logic below:
+    tasks.forEach(element => element.parentElement.addEventListener('click', function () {
+        completeTask(element.parentElement.className);
+    }))
+}
+
+function addTaskListener() {
+    let input = document.getElementById('submit');
+
+    input.addEventListener('click',function (e) {
+        let title = document.getElementById("newtask").value;
+        let taskListId = document.getElementById("list").value;
+        addTask(taskListId,title);
+    })
+}
+
+function addSelectOnChange() {
+    let select = document.getElementById('list');
+
+    select.addEventListener('change', function () {
+        printTasks();
+    })
+}
+
+function checkTasklistOrder(taskslists) {
+    let taskListsCache = sessionStorage.getItem('tasklists');
+    let select = document.getElementById('list');
+
+    if ( !(JSON.stringify(taskslists) === taskListsCache)){
+        sessionStorage.setItem('tasklists', JSON.stringify(taskslists));
+
+        let child = select.lastElementChild;
+        while (child) {
+            select.removeChild(child);
+            child = select.lastElementChild;
+        }
+
+        taskslists.items.forEach(element => {
+            let option = document.createElement('option');
+            option.appendChild(document.createTextNode(element.title));
+            option.value = element.id;
+            select.appendChild(option);
+        });
+    }
 }
